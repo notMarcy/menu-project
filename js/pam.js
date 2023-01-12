4b3fd244ebc9b4d9f0927a4c0765817a35b0ec7f
Kartohha <sivyhaalexeu.228@gmail.com>
2023-01-12 21:18:34 +0300

    end commit

--- js/pam.js
+++ js/pam.js
@@ -1,14 +1,163 @@
 let german = document.querySelector('.ger');
 let germinf = document.querySelector('.german');
 let sh=true;
+let v = document.querySelector('.cont')
 german.addEventListener("click", function(){
   
    if(sh==true){
          germinf.style.zIndex ="3"
          sh = false
+         german.style.background = "#4e4d4d"
    }
    else if(sh==false){
       germinf.style.zIndex="1"
       sh=true
+      german.style.background = "#696969"
+      v.style.background ="#696969"
+   }
+})
+let grec = document.querySelector('.gre');
+let grecinf = document.querySelector('.greс');
+let sh1=true;
+grec.addEventListener("click", function(){
+   if(sh1==true){
+         grecinf.style.zIndex ="3"
+         sh1 = false
+         grec.style.background = "#4e4d4d"
+   }
+   else if(sh1==false){
+      grecinf.style.zIndex="1"
+      sh1=true
+      grec.style.background = "#696969"
+   }
+})
+let gryz = document.querySelector('.gry');
+let gryinf = document.querySelector('.gryz');
+let sh2=true;
+gryz.addEventListener("click", function(){
+   if(sh2==true){
+         gryinf.style.zIndex ="3"
+         sh2 = false
+         gryz.style.background = "#4e4d4d"
+   }
+   else if(sh2==false){
+      gryinf.style.zIndex="1"
+      sh2=true
+      gryz.style.background = "#696969"
+   }
+})
+
+let egypt = document.querySelector('.egy');
+let egyinf = document.querySelector('.egypt');
+let sh3=true;
+egypt.addEventListener("click", function(){
+   if(sh3==true){
+         egyinf.style.zIndex ="3"
+         sh3 = false
+         egypt.style.background = "#4e4d4d"
+   }
+   else if(sh3==false){
+      egyinf.style.zIndex="1"
+      sh3=true
+      egypt.style.background = "#696969"
+   }
+})
+
+let isp = document.querySelector('.isp');
+let ispan = document.querySelector('.ispan');
+let sh4=true;
+isp.addEventListener("click", function(){
+   if(sh4==true){
+         ispan.style.zIndex ="3"
+         sh4 = false
+         isp.style.background = "#4e4d4d"
+   }
+   else if(sh4==false){
+      ispan.style.zIndex="1"
+      sh4=true
+      isp.style.background = "#696969"
+   }
+})
+
+let nor = document.querySelector('.nor');
+let norinf = document.querySelector('.norveg');
+let sh5=true;
+nor.addEventListener("click", function(){
+   if(sh5==true){
+         norinf.style.zIndex ="3"
+         sh5 = false
+         nor.style.background = "#4e4d4d"
+   }
+   else if(sh5==false){
+      norinf.style.zIndex="1"
+      sh5=true
+      nor.style.background = "#696969"
+   }
+})
+
+
+let tyr = document.querySelector('.tyr');
+let tyrinf = document.querySelector('.tyrk');
+let sh6=true;
+tyr.addEventListener("click", function(){
+   if(sh6==true){
+         tyrinf.style.zIndex ="3"
+         sh6 = false
+         tyr.style.background = "#4e4d4d"
+   }
+   else if(sh6==false){
+      tyrinf.style.zIndex="1"
+      sh6=true
+      tyr.style.background = "#696969"
+   }
+})
+
+
+let shve = document.querySelector('.shve');
+let shinf= document.querySelector('.shvec');
+let sh7=true;
+shve.addEventListener("click", function(){
+   if(sh7==true){
+         shinf.style.zIndex ="3"
+         sh7 = false
+         shve.style.background = "#4e4d4d"
+   }
+   else if(sh7==false){
+      shinf.style.zIndex="1"
+      sh7=true
+      shve.style.background = "#696969"
+   }
+})
+
+
+let shri = document.querySelector('.shri');
+let shriinf = document.querySelector('.shrilank');
+let sh8=true;
+shri.addEventListener("click", function(){
+   if(sh8==true){
+         shriinf.style.zIndex ="3"
+         sh8 = false
+         shri.style.background = "#4e4d4d"
+   }
+   else if(sh8==false){
+      shriinf.style.zIndex="1"
+      sh8=true
+      shri.style.background = "#696969"
+   }
+})
+
+let jap = document.querySelector('.iap');
+let japinf = document.querySelector('.japon');
+let sh9=true;
+jap.addEventListener("click", function(){
+   if(sh9==true){
+         japinf.style.zIndex ="3"
+         sh9 = false
+         jap.style.background = "#4e4d4d"
+   }
+   else if(sh9==false){
+      japinf.style.zIndex="1"
+      sh9=true
+      jap.style.background = "#696969"
    }
 })
